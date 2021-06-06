46b2d3d7440828781b8dcf66f756f9f1b0709a74
mrinalmech <mrinalmech@gmail.com>
2021-06-07 04:12:24 +0530

    Add gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,9 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     /*{
       resolve: `gatsby-plugin-offline`,
       options: {
