98f6c633f26444deec36598c443b6adff93a74fb
Mrinal Mech <mrinalmech@gmail.com>
2023-11-08 04:37:16 +0530

    Added tests for fragments

--- src/components/atoms/LoadedImg/LoadedImg.test.tsx
+++ src/components/atoms/LoadedImg/LoadedImg.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import * as Gatsby from 'gatsby';
 
 import LoadedImg from './LoadedImg';
+import { AppReadyContext } from '../../../contexts/AppReadyContext';
 
 const useStaticQuery = jest.spyOn(Gatsby, 'useStaticQuery');
 const mockUseStaticQuery = {
@@ -31,5 +32,14 @@ describe('LoadedImg', () => {
   test('Expect LoadedImg to be not presented if image not in filesystem', () => {
     render(<LoadedImg imgName="testImg2" alt="testImgAlt" />);
     expect(screen.queryByAltText(/testImgAlt/i)).not.toBeInTheDocument();
+  });
+
+  test('Expect LoadedImg to be not visible if app is loaded', () => {
+    render(
+      <AppReadyContext.Provider value={true}>
+        <LoadedImg imgName="testImg" alt="testImgAlt" />
+      </AppReadyContext.Provider>,
+    );
+    expect(screen.queryByAltText(/testImgAlt/i)?.parentElement).toHaveClass('animFinal');
   });
 });
---
+++ src/components/layouts/Page/Page.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import Page from './Page';
+
+describe('Footer', () => {
+  test('Expect page to be presented', () => {
+    const currentYear = new Date().getFullYear();
+
+    render(<Page>Test content</Page>);
+
+    expect(screen.getByText(/Test content/i)).toBeInTheDocument();
+
+    expect(screen.getByAltText(/Company Logo/i)).toBeInTheDocument();
+    expect(screen.getByAltText(/Company Logo/i).parentElement).toHaveAttribute('href', '/');
+    expect(screen.getByRole('link', { name: /Blog/i })).toHaveAttribute(
+      'href',
+      'https://blog.macondogames.com/',
+    );
+    expect(screen.getByRole('link', { name: /Press/i })).toHaveAttribute('href', '/press');
+    expect(screen.getByRole('link', { name: /Contact/i })).toHaveAttribute(
+      'href',
+      'mailto:info@macondogames.com',
+    );
+    expect(screen.getByRole('link', { name: /Steam/i })).toHaveAttribute(
+      'href',
+      'https://store.steampowered.com/app/1073970/Global_Steel/',
+    );
+
+    expect(screen.getByText(/Keep up with us!/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(`Copyright © ${currentYear} | Macondo Games Pvt. Ltd.`),
+    ).toBeInTheDocument();
+
+    expect(screen.getAllByRole('link', { name: /Twitter/i })).toHaveLength(2);
+    expect(screen.getAllByRole('link', { name: /Facebook/i })).toHaveLength(2);
+    expect(screen.getAllByRole('link', { name: /Instagram/i })).toHaveLength(2);
+    expect(screen.getAllByRole('link', { name: /Youtube/i })).toHaveLength(2);
+    expect(screen.getAllByRole('link', { name: /Discord/i })).toHaveLength(2);
+    expect(screen.getAllByRole('link', { name: /Twitch/i })).toHaveLength(2);
+  });
+});
