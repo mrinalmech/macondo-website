b3ec633bb86b992cc34cf3619d907d6649287657
Mrinal Mech <mrinalmech@gmail.com>
2024-02-24 05:08:23 +0530

    Fix

--- src/components/fragments/Header/Header.test.tsx
+++ src/components/fragments/Header/Header.test.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  act,
-  render,
-  screen,
-  fireEvent,
-  waitForElementToBeRemoved,
-  queryByTestId,
-} from '@testing-library/react';
+import { act, render, screen, fireEvent, waitForElementToBeRemoved } from '@testing-library/react';
 
 import Header from './Header';
 
